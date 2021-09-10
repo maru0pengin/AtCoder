10457e0518418b1f8dc0f098f1f149339c969f47
maru0pengin <maru0pengin@gmail.com>
2021-09-11 01:54:56 +0900

    203のC問題を勉強

--- 204/C.py
+++ 204/C.py
@@ -49,25 +49,22 @@
 
 # print(ans)
   # おまじない
-
 import sys
 sys.setrecursionlimit(10000)
 
 # 入力の読み込み
 N,M=map(int,input().split())
-G=[-1 for _ in range(N)]
+G=[[] for i in range(N)]
 # G[i] は都市iから道路で直接繋がっている都市のリスト
 for i in range(M):
   A,B=map(int,input().split())
-  G[A-1]=(B-1)
+  G[A-1].append(B-1)
 
 # dfs
 def dfs(v):
   if temp[v]: return  # 同じ頂点を2度以上調べないためのreturn
   temp[v]=True
-  #for vv in G[v]: dfs(vv)
-  if G[v] > 0:
-    dfs(G[v])
+  for vv in G[v]: dfs(vv)
 
 ans=0
 
