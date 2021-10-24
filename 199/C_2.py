e74e637b9bf9dff25eb9db59bf3326aa8a34d53e
maru0pengin <maru0pengin@gmail.com>
2021-10-24 19:35:06 +0900

    199_Cの答えを見て３完

--- 199/C_2.py
+++ 199/C_2.py
@@ -12,15 +12,25 @@ for i in range(Q):
     b = 0
     if T[i] == 1:
         if flag:
-            if A[i]-1 > N:
+            # print("hoge")
+            if A[i]-1 >= N:
                 a = A[i]-1 - N
             else:
-                a = A[i]
-
+                a = A[i]-1 + N
+            if B[i]-1 >= N:
+                b = B[i]-1 - N
             else:
+                b = B[i]-1 + N
+            S[a], S[b] = S[b], S[a]
+
+        else:
             S[A[i]-1], S[B[i]-1] = S[B[i]-1], S[A[i]-1]
     else:
-        S1, S2 = S2, S1
+        flag = not flag
+    # print(S)
+
+if flag:
+    S[:N], S[N:] = S[N:], S[:N]
 
 s = ''.join(S)
 print(s)
